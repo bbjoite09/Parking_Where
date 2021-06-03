3a2be401041e817a7918834efd963e78f48d66cf
Heeeon <bbjoite09@naver.com>
2021-06-03 10:17:13 +0900

    DB 위도, 경도 정보 추가

--- backend/app.py
+++ backend/app.py
@@ -13,9 +13,11 @@ app = Flask(__name__)
 load_dotenv()
 SEOUL_API_KEY = os.environ['SEOUL_API_KEY']
 
+
 @app.route('/')
 def home():
     return 'main page'
+
 
 # 공공데이터 공영주차장 정보 DB에 저장
 @app.route('/api/public_plot', methods=['GET'])
@@ -29,7 +31,7 @@ def get_parking_lots():
     datas = result['GetParkInfo']['row']
 
     for data in datas:
-        # 필요한 정보 정의(주차장명, 주소, 유/무료 구분, 야간 무료 여부, 기본 요금, 기본 시간, 추가 요금, 주간 시작 시간, 주간 종료 시간)
+        # 필요한 정보 정의(주차장명, 주소, 유/무료 구분, 야간 무료 여부, 기본 요금, 기본 시간, 추가 요금, 주간 시작 시간, 주간 종료 시간, 위도, 경도)
         name = data['PARKING_NAME']
         address = data['ADDR']
         free = data['PAY_YN']
@@ -42,6 +44,9 @@ def get_parking_lots():
         weekday_begin_time = data['WEEKDAY_BEGIN_TIME']
         weekday_end_time = data['WEEKDAY_END_TIME']
 
+        lat = data['LAT']
+        lng = data['LNG']
+
         doc = {
             "Name": name,
             "Address": address,
@@ -49,15 +54,18 @@ def get_parking_lots():
             "Night free": night_free,
             "Basic_cost": basic_cost,
             "Basic_time": basic_time,
-            "Add cost" : add_cost,
+            "Add cost": add_cost,
             "Weekday begin time": weekday_begin_time,
-            "Weekday end time": weekday_end_time
+            "Weekday end time": weekday_end_time,
+            "Latitude": lat,
+            "Longitude": lng
         }
 
         # document 삽입
         db.park_info.insert(doc)
 
     return 'DB 삽입 성공'
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=7000, debug=True)
