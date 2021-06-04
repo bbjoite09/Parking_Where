672dd8e6482f9cd8c94ab9f2b5a01dcdebebc9f4
wil953742 <wil953742@gmail.com>
2021-06-05 00:50:59 +0900

    front-end 작업4

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -23,7 +23,9 @@ export default function Search() {
             <button
                 className={style.search_button}
                 type="submit"
-            />
+            >
+                검색
+            </button>
         </div>
     )
 }
