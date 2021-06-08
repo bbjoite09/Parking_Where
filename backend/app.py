661ca07f0335aa11233a66f50f518b1011439f6e
Sion Noh <shtldhs123@ajou.ac.kr>
2021-06-09 01:27:23 +0900

    json 파일추가 및 db로 주차장 정보 저장하기

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import pymongo
@@ -16,63 +17,69 @@ app.config["DEBUG"] = True
 load_dotenv()
 SEOUL_API_KEY = os.environ['SEOUL_API_KEY']
 
+
 @app.route('/', methods=["GET", "POST"])
 def home():
     return 'main page'
 
+
 @app.route('/search', methods=["POST"])
-def search() :
+def search():
     search_data = request.get_json()
     print(search_data)
 
-# 공공데이터 공영주차장 정보 DB에 저장
-@app.route('/api/public_plot', methods=['GET'])
-def get_parking_lots():
-    response = requests.get(
-        f'http://openapi.seoul.go.kr:8088/{SEOUL_API_KEY}/json/GetParkInfo/1/1000/'
-    )
 
-    result = response.json()
+# 공공데이터 공영주차장 정보 DB에 저장 (총 14417개)
+@app.route('/api/data')
+def get_data():
+    with open('static/seoul_park_lot.json', encoding='UTF8') as json_file:
+        result = json.load(json_file)
 
-    datas = result['GetParkInfo']['row']
+        datas = result["DATA"]
 
-    for data in datas:
-        # 필요한 정보 정의(주차장명, 주소, 유/무료 구분, 야간 무료 여부, 기본 요금, 기본 시간, 추가 요금, 주간 시작 시간, 주간 종료 시간, 위도, 경도)
-        name = data['PARKING_NAME']
-        address = data['ADDR']
-        free = data['PAY_YN']
-        night_free = data['NIGHT_FREE_OPEN']
+        count = 0
+        for data in datas:
+            # 필요한 정보 정의(주차장명, 주소, 유/무료 구분, 야간 무료 여부, 기본 요금, 기본 시간, 추가 요금, 주간 시작 시간, 주간 종료 시간, 위도, 경도)
+            park_id = count
+            name = data['parking_name']
+            address = data['addr']
+            free = data['pay_nm']
+            night_free = data['night_free_open']
 
-        basic_cost = data['RATES']
-        basic_time = data['TIME_RATE']
-        add_cost = data['ADD_RATES']
+            basic_cost = data['rates']
+            basic_time = data['time_rate']
+            add_cost = data['add_rates']
 
-        weekday_begin_time = data['WEEKDAY_BEGIN_TIME']
-        weekday_end_time = data['WEEKDAY_END_TIME']
+            weekday_begin_time = data['weekday_begin_time']
+            weekday_end_time = data['weekday_end_time']
 
-        lat = data['LAT']
-        lng = data['LNG']
+            lat = data['lat']
+            lng = data['lng']
 
-        doc = {
-            "Name": name,
-            "Address": address,
-            "Free": free,
-            "Night free": night_free,
-            "Basic_cost": basic_cost,
-            "Basic_time": basic_time,
-            "Add cost": add_cost,
-            "Weekday begin time": weekday_begin_time,
-            "Weekday end time": weekday_end_time,
-            "location": {
-                "type": 'Point',
-                "coordinates": [lng, lat]  # [경도,위도] 순서
-            },
-        }
+            doc = {
+                "park_id": park_id,
+                "Name": name,
+                "Address": address,
+                "Free": free,
+                "Night free": night_free,
+                "Basic_cost": basic_cost,
+                "Basic_time": basic_time,
+                "Add cost": add_cost,
+                "Weekday begin time": weekday_begin_time,
+                "Weekday end time": weekday_end_time,
+                "location": {
+                    "type": 'Point',
+                    "coordinates": [lng, lat]  # [경도,위도] 순서
+                },
+            }
+            count += 1
+            # document 삽입
+            db.park_info.insert(doc)
 
-        # document 삽입
-        db.park_info.insert(doc)
+    print(db.park_info.count())
 
     return 'DB 삽입 성공'
+
 
 @app.route('/api/public_plot/get', methods=['GET'])
 def get_index():
@@ -82,21 +89,20 @@ def get_index():
 
     # 예시: 사각형 안에 포함되는 주차장
     results = list(db.park_info.find({
-       'location': {
-          '$geoWithin': {
-             '$geometry': {
-               'type': "Polygon" ,
-               'coordinates': [[[ 0, 0 ],
-                               [ 130, 0],
-                                [ 130, 50 ],
-                                [ 0, 50 ],
-                                [0, 0]]]
-           }}}}))
+        'location': {
+            '$geoWithin': {
+                '$geometry': {
+                    'type': "Polygon",
+                    'coordinates': [[[0, 0],
+                                     [130, 0],
+                                     [130, 50],
+                                     [0, 50],
+                                     [0, 0]]]
+                }}}}))
     for i in results[:3]:
         print(i)
 
     return '1'
-
 
 
 if __name__ == '__main__':
